665a56c0ebf18ed92c992b3d990118b30c066d7f
nearih <raevoli@gmail.com>
2018-08-04 16:22:26 +0700

    draft

--- api/main.go
+++ api/main.go
@@ -1,7 +1,198 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"sort"
+	"strings"
+
+	"github.com/julienschmidt/httprouter"
+	tf "github.com/tensorflow/tensorflow/tensorflow/go"
+	"github.com/tensorflow/tensorflow/tensorflow/go/op"
+)
+
+type ClassifyResult struct {
+	Filename string        `json:"filename"`
+	Labels   []LabelResult `json:"labels"`
+}
+
+type LabelResult struct {
+	Label       string  `json:"label"`
+	Probability float32 `json:"probability"`
+}
+
+var (
+	graphModel   *tf.Graph
+	sessionModel *tf.Session
+	labels       []string
+)
 
 func main() {
-	fmt.Println("Hello, Gophers")
+	if err := loadModel(); err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	r := httprouter.New()
+	r.POST("/recognize", recognizeHandler)
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func loadModel() error {
+	// Load inception model
+	model, err := ioutil.ReadFile("/model/tensorflow_inception_graph.pb")
+	if err != nil {
+		return err
+	}
+	graphModel = tf.NewGraph()
+	if err := graphModel.Import(model, ""); err != nil {
+		return err
+	}
+
+	sessionModel, err = tf.NewSession(graphModel, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Load labels
+	labelsFile, err := os.Open("/model/imagenet_comp_graph_label_strings.txt")
+	if err != nil {
+		return err
+	}
+	defer labelsFile.Close()
+	scanner := bufio.NewScanner(labelsFile)
+	// Labels are separated by newlines
+	for scanner.Scan() {
+		labels = append(labels, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func recognizeHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	// Read image
+	imageFile, header, err := r.FormFile("image")
+	// Will contain filename and extension
+	imageName := strings.Split(header.Filename, ".")
+	if err != nil {
+		responseError(w, "Could not read image", http.StatusBadRequest)
+		return
+	}
+	defer imageFile.Close()
+	var imageBuffer bytes.Buffer
+	// Copy image data to a buffer
+	io.Copy(&imageBuffer, imageFile)
+
+	// ...
+	// Make tensor
+	tensor, err := makeTensorFromImage(&imageBuffer, imageName[:1][0])
+	if err != nil {
+		responseError(w, "Invalid image", http.StatusBadRequest)
+		return
+	}
+
+	// Run inference
+	output, err := sessionModel.Run(
+		map[tf.Output]*tf.Tensor{
+			graphModel.Operation("input").Output(0): tensor,
+		},
+		[]tf.Output{
+			graphModel.Operation("output").Output(0),
+		},
+		nil)
+	if err != nil {
+		responseError(w, "Could not run inference", http.StatusInternalServerError)
+		return
+	}
+
+	// Return best labels
+	responseJSON(w, ClassifyResult{
+		Filename: header.Filename,
+		Labels:   findBestLabels(output[0].Value().([][]float32)[0]),
+	})
+}
+
+type ByProbability []LabelResult
+
+func (a ByProbability) Len() int           { return len(a) }
+func (a ByProbability) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByProbability) Less(i, j int) bool { return a[i].Probability > a[j].Probability }
+
+func findBestLabels(probabilities []float32) []LabelResult {
+	// Make a list of label/probability pairs
+	var resultLabels []LabelResult
+	for i, p := range probabilities {
+		if i >= len(labels) {
+			break
+		}
+		resultLabels = append(resultLabels, LabelResult{Label: labels[i], Probability: p})
+	}
+	// Sort by probability
+	sort.Sort(ByProbability(resultLabels))
+	// Return top 5 labels
+	return resultLabels[:5]
+}
+
+func makeTensorFromImage(imageBuffer *bytes.Buffer, imageFormat string) (*tf.Tensor, error) {
+	tensor, err := tf.NewTensor(imageBuffer.String())
+	if err != nil {
+		return nil, err
+	}
+	graph, input, output, err := makeTransformImageGraph(imageFormat)
+	if err != nil {
+		return nil, err
+	}
+	session, err := tf.NewSession(graph, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer session.Close()
+	normalized, err := session.Run(
+		map[tf.Output]*tf.Tensor{input: tensor},
+		[]tf.Output{output},
+		nil)
+	if err != nil {
+		return nil, err
+	}
+	return normalized[0], nil
+}
+
+// Creates a graph to decode, rezise and normalize an image
+func makeTransformImageGraph(imageFormat string) (graph *tf.Graph, input, output tf.Output, err error) {
+	const (
+		H, W  = 224, 224
+		Mean  = float32(117)
+		Scale = float32(1)
+	)
+	s := op.NewScope()
+	input = op.Placeholder(s, tf.String)
+	// Decode PNG or JPEG
+	var decode tf.Output
+	if imageFormat == "png" {
+		decode = op.DecodePng(s, input, op.DecodePngChannels(3))
+	} else {
+		decode = op.DecodeJpeg(s, input, op.DecodeJpegChannels(3))
+	}
+	// Div and Sub perform (value-Mean)/Scale for each pixel
+	output = op.Div(s,
+		op.Sub(s,
+			// Resize to 224x224 with bilinear interpolation
+			op.ResizeBilinear(s,
+				// Create a batch containing a single image
+				op.ExpandDims(s,
+					// Use decoded pixel values
+					op.Cast(s, decode, tf.Float),
+					op.Const(s.SubScope("make_batch"), int32(0))),
+				op.Const(s.SubScope("size"), []int32{H, W})),
+			op.Const(s.SubScope("mean"), Mean)),
+		op.Const(s.SubScope("scale"), Scale))
+	graph, err = s.Finalize()
+	return graph, input, output, err
 }
